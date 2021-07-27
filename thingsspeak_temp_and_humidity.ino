151b204d0c5ce23fb4aa87d21573d61d21a42f38
Paulo Ceccon <pauloceccon2012@hotmail.com>
2021-07-27 18:14:53 -0300

    Add code

---
+++ thingsspeak_temp_and_humidity.ino
@@ -0,0 +1,101 @@
+#include <SoftwareSerial.h>
+#include <dht11.h>
+#define RX 2
+#define TX 3
+#define dht_apin 11 // Analog Pin sensor is connected to
+dht11 dhtObject;
+String AP = "NASCIMENTO";       // AP NAME
+String PASS = "20092010"; // AP PASSWORD
+String API = "MZ6VXUM0LIVPAOS5";   // Write API KEY
+String HOST = "api.thingspeak.com";
+String PORT = "80";
+int countTrueCommand;
+int countTimeCommand; 
+boolean found = false; 
+int valSensor = 1;
+  
+SoftwareSerial esp8266(RX,TX); 
+  
+void setup() {
+  Serial.begin(9600);
+  esp8266.begin(115200);
+  sendCommand("AT",5,"OK");
+  sendCommand("AT+CWMODE=1",5,"OK");
+  sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",20,"OK");
+}
+
+ void loop() {
+  
+ String getData = "GET /update?api_key="+ API +"&field1="+getTemperatureValue()+"&field2="+getHumidityValue();
+ delay(15000);
+ sendCommand("AT+CIPMUX=1",5,"OK");
+ delay(500);
+ sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
+ delay(500);
+ sendCommand("AT+CIPSEND=0," +String(getData.length()+4),4,">");
+ delay(5000);
+ esp8266.println(getData);
+ delay(15000);
+ countTrueCommand++;
+ delay(500);
+ sendCommand("AT+CIPCLOSE=0",5,"OK");
+ delay(500);
+}
+
+
+String getTemperatureValue(){
+
+   dhtObject.read(dht_apin);
+   Serial.print(" Temperature(C)= ");
+   int temp = dhtObject.temperature;
+   Serial.println(temp); 
+   delay(500);
+   return String(temp); 
+  
+}
+
+
+String getHumidityValue(){
+
+   dhtObject.read(dht_apin);
+   Serial.print(" Humidity in %= ");
+   int humidity = dhtObject.humidity;
+   Serial.println(humidity);
+   delay(500);
+   return String(humidity); 
+  
+}
+
+void sendCommand(String command, int maxTime, char readReplay[]) {
+  Serial.print(countTrueCommand);
+  Serial.print(". at command => ");
+  Serial.print(command);
+  Serial.print(" ");
+  while(countTimeCommand < (maxTime*1))
+  {
+    esp8266.println(command);//at+cipsend
+    if(esp8266.find(readReplay))//ok
+    {
+      found = true;
+      break;
+    }
+  
+    countTimeCommand++;
+  }
+  
+  if(found == true)
+  {
+    Serial.println("OYI");
+    countTrueCommand++;
+    countTimeCommand = 0;
+  }
+  
+  if(found == false)
+  {
+    Serial.println("Fail");
+    countTrueCommand = 0;
+    countTimeCommand = 0;
+  }
+  
+  found = false;
+ }
